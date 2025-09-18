197bb65e33ca35684a98fb54489f9ac4f98dc5be
Joseph Skubal <joseph.skubal@summaft.com>
2025-09-18 12:59:38 -0500

    first draft

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use clap::{Parser, ValueEnum};
+use std::io::stdin;
+use std::iter;
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum Case {
+    LowerCamel,
+    UpperCamel,
+    Snake,
+    ScreamingSnake,
+    Kebab,
+    ScreamingKebab,
+}
+
+/// Read newline delimited names from stdin, transform to the given case and write them to stdout
+///
+/// If you are running the program in "interactive" mode (you did not pipe input into the program),
+/// use Ctrl+D to close stdin and quit.
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// The case to be read
+    pub case: Case,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    for line in stdin().lines() {
+        let line = line?;
+        let tokens = tokenize(&line);
+        println!("{}", recombine(&tokens, args.case));
+    }
+
+    Ok(())
+}
+
+fn tokenize(input: &str) -> Vec<String> {
+    let split_indices: Vec<usize> = input
+        .chars()
+        .zip(input.char_indices().skip(1))
+        .filter_map(|(left, (idx, right))| {
+            if should_split(left, right) {
+                Some(idx)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut input = input;
+    let mut tokens = Vec::with_capacity(split_indices.len() + 1);
+    for index in split_indices.into_iter().rev() {
+        let (left, right) = input.split_at(index);
+        input = left;
+        tokens.push(normalize(right));
+    }
+    tokens.push(normalize(input));
+    tokens.reverse();
+    tokens
+}
+
+fn recombine(tokens: &[String], format: Case) -> String {
+    match format {
+        Case::LowerCamel => {
+            if let Some((first, rest)) = tokens.split_first() {
+                iter::once(first.to_owned())
+                    .chain(rest.into_iter().map(|t| capitalize_first(t)))
+                    .collect()
+            } else {
+                String::new()
+            }
+        }
+        Case::UpperCamel => tokens.into_iter().map(|t| capitalize_first(t)).collect(),
+        Case::Snake => tokens.join("_"),
+        Case::ScreamingSnake => tokens.join("_").to_ascii_uppercase(),
+        Case::Kebab => tokens.join("-"),
+        Case::ScreamingKebab => tokens.join("-").to_uppercase(),
+    }
+}
+
+fn should_split(left: char, right: char) -> bool {
+    return left.is_ascii_lowercase() && right.is_ascii_uppercase()
+        || !is_terminator(left) && is_terminator(right);
+}
+
+fn is_terminator(c: char) -> bool {
+    matches!(c, '_' | '-' | ' ')
+}
+
+fn normalize(token: &str) -> String {
+    token
+        .trim()
+        .chars()
+        .filter(|&c| !is_terminator(c))
+        .map(|c| c.to_ascii_lowercase())
+        .collect()
+}
+
+// Return a copy of this token with the first character capitalized
+fn capitalize_first(token: &str) -> String {
+    let mut chars_iter = token.chars();
+    let first = chars_iter.next();
+
+    match first {
+        Some(ch) => iter::once(ch.to_ascii_uppercase())
+            .chain(chars_iter)
+            .collect(),
+        None => String::new(),
+    }
+}
